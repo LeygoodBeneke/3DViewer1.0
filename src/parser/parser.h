8f987a923048b91d02ab64d386c558be6e30ef3b
Sippydon <mellyoker@gmail.com>
2023-10-28 00:36:03 +0300

    redesigned the parser, removed all strtok func because they caused memory errors during frequent accesses

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -20,18 +20,6 @@ typedef struct {
     int a;
     int b;
 }line;
-/*
-void parser(char *file_path, point **points, int *points_len, surface **surf, int *surf_len);
-int is_coordinate (char *line, int *trigger);
-void create_point(point* p, char* line);
-surface *get_surface(point *p, int length, FILE *fp, int number_surfaces, point *points);
-int is_surface(char* line);
-void create_surface(surface *surf, char *line, point *points);
-int number_of_coordinates(char *line);
-void number_of_points_and_surfaces(int* number_points, int* number_planes, FILE* fp);
- */
-
-/**************************** Вариант с Line ***************************************/
 
 void parser(char *file_path, point **points, int *points_len, line **lines, int *surf_len);
 int is_coordinate (char *line, int *trigger);
@@ -39,7 +27,9 @@ void create_point(point* p, char* line);
 line *get_lines(FILE *fp, int *number_surfaces);
 int is_surface(char* str);
 void create_line(line *lines, char *str, int *i);
-int number_of_coordinates(char *line);
 void number_of_points_and_surfaces(int* number_points, int* number_surfaces, FILE* fp);
+
+char* get_token(char *str);
+int get_point_number(char *token);
 
 #endif //C8_3DVIEWER_V1_0_2_PARSER_H
