69e8969c76fb42e521c9d8e04de47f2ab4f72733
Sippydon <mellyoker@gmail.com>
2023-10-30 01:02:42 +0300

    add tests and Makefile

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -1,26 +1,26 @@
 #ifndef C8_3DVIEWER_V1_0_2_PARSER_H
 #define C8_3DVIEWER_V1_0_2_PARSER_H
 
-#include "string.h"
 #include "stdio.h"
 #include "stdlib.h"
 
 typedef struct {
-    double x;
-    double y;
-    double z;
-}point;
+  double x;
+  double y;
+  double z;
+} point;
 
 typedef struct {
-    point *points;
-    int len;
-}surface;
+  point *points;
+  int len;
+} surface;
 
 typedef struct {
-    int a;
-    int b;
-}line;
+  int a;
+  int b;
+} line;
 
-void parser(char *file_path, point **points, int *points_len, line **lines, int *surface_len);
+void parser(char *file_path, point **points, int *points_len, line **lines,
+            int *surface_len);
 
-#endif //C8_3DVIEWER_V1_0_2_PARSER_H
+#endif  // C8_3DVIEWER_V1_0_2_PARSER_H
