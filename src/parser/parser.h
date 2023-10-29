efc84e1506bff8f75ad313736bf9d3ed2b2c079a
Sippydon <mellyoker@gmail.com>
2023-10-29 21:36:23 +0300

    cleanup code

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -21,15 +21,6 @@ typedef struct {
     int b;
 }line;
 
-void parser(char *file_path, point **points, int *points_len, line **lines, int *surf_len);
-int is_coordinate (char *line, int *trigger);
-void create_point(point* p, char* line);
-line *get_lines(FILE *fp, int *number_surfaces);
-int is_surface(char* str);
-void create_line(line *lines, char *str, int *i);
-void number_of_points_and_surfaces(int* number_points, int* number_surfaces, FILE* fp);
-
-char* get_token(char *str);
-int get_point_number(char *token);
+void parser(char *file_path, point **points, int *points_len, line **lines, int *surface_len);
 
 #endif //C8_3DVIEWER_V1_0_2_PARSER_H
